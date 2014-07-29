6e17e74652e3685a30c207e26db3819284e131ec
lizzerdrix <liz@lizbaillie.com>
2014-07-29 14:58:40 -0400

    removes commas from CreateRatSighting migration

--- rattracker/db/migrate/20140729185518_create_rat_sightings.rb
+++ rattracker/db/migrate/20140729185518_create_rat_sightings.rb
@@ -1,11 +1,11 @@
 class CreateRatSightings < ActiveRecord::Migration
   def change
     create_table :rat_sightings do |t|
-      t.float, :latitude
-      t.float, :longitude
-      t.datetime, :created_date
-      t.string :location_type,
-      t.integer, :zip
+      t.float :latitude
+      t.float :longitude
+      t.datetime :created_date
+      t.string :location_type
+      t.integer :zip
       t.string :address
 
       t.timestamps
