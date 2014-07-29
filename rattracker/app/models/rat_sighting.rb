f716db1570fd62bb9f04728fcc1636f03c1ad8b6
Justin Kim <justinjmkim@gmail.com>
2014-07-29 17:02:06 -0400

    Connect model, view, and controller to display all addresses with input zip

--- rattracker/app/models/rat_sighting.rb
+++ rattracker/app/models/rat_sighting.rb
@@ -1,4 +1,9 @@
 class RatSighting < ActiveRecord::Base
 
-    
+  def self.zip_specific_sightings(params_zip)
+    RatSighting.all.select do |rat_sighting|
+      rat_sighting.zip == params_zip.to_i
+    end
+  end
+
 end
