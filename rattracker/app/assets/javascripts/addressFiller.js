1bdc9759c651575e4ee55f07245cac1ec8966eb2
lizzerdrix <liz@lizbaillie.com>
2014-07-29 17:41:54 -0400

    edits addressFiller

--- rattracker/app/assets/javascripts/addressFiller.js
+++ rattracker/app/assets/javascripts/addressFiller.js
@@ -3,39 +3,42 @@
 //if address field is empty, use lat/long to populate it 
 //     -reverse geocaching to grab address
 
-  var geocoder;
-  var map;
-  var infowindow = new google.maps.InfoWindow();
-  var marker;
-  function initialize() {
-    geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(40.730885,-73.997383);
-    var mapOptions = {
-      zoom: 8,
-      center: latlng
-    }
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  }
 
-  function codeLatLng() {
-    var input = document.getElementById("latlng").value;
-    var latlngStr = input.split(",",2);
-    var lat = parseFloat(latlngStr[0]);
-    var lng = parseFloat(latlngStr[1]);
-    var latlng = new google.maps.LatLng(lat, lng);
-    geocoder.geocode({'latLng': latlng}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        if (results[1]) {
-          map.setZoom(11);
-          marker = new google.maps.Marker({
-              position: latlng,
-              map: map
-          });
-          infowindow.setContent(results[1].formatted_address);
-          infowindow.open(map, marker);
-        }
-      } else {
-        alert("Geocoder failed due to: " + status);
+(function codeLatLng() {
+  var geocoder = new google.maps.Geocoder();
+  var lat = parseFloat(latlngStr[0]);
+  var lng = parseFloat(latlngStr[1]);
+  var latlng = new google.maps.LatLng(lat, lng);
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+
       }
-    });
-  }
+    } else {
+      alert("Geocoder failed due to: " + status);
+    }
+  });
+})()
+
+// $.ajax({
+//   url: "/lists.json",
+//   dataType: "JSON",
+//   success: function(response){
+//     response.forEach(function(obj){
+//       that.createList(obj);
+//     });
+//   },
+//   error: function(){alert("failure!");}
+// });
+
+// $.ajax({
+//   url: "/lists/" + listId,
+//   type: "DELETE",
+//   dataType: "JSON",
+//   data: {list: {id: listId}},
+//   success: function(){
+//     $deletedList.closest(".list").remove();
+//     List.delete(listId);
+//   },
+//   error: function(){alert("failure!");}
+// });
