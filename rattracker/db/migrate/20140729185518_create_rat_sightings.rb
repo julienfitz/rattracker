6e8567dc261512c56fafa7de014412ecc6a77c75
Justin Kim <justinjmkim@gmail.com>
2014-07-29 16:18:31 -0400

    Create seed file

--- rattracker/db/migrate/20140729185518_create_rat_sightings.rb
+++ rattracker/db/migrate/20140729185518_create_rat_sightings.rb
@@ -3,7 +3,7 @@ class CreateRatSightings < ActiveRecord::Migration
     create_table :rat_sightings do |t|
       t.float :latitude
       t.float :longitude
-      t.datetime :created_date
+      t.string :created_date
       t.string :location_type
       t.integer :zip
       t.string :address
