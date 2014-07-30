7c6c4577fa02c3251112d305adbc00ad7ee961d6
bshore01 <benjamin.shore@flatironschool.com>
2014-07-30 13:45:48 -0400

    semi working reverse geocoding

--- rattracker/app/assets/javascripts/addressFiller.js
+++ rattracker/app/assets/javascripts/addressFiller.js
@@ -5,40 +5,55 @@
 
 function codeLatLng() {
   console.log("This is running");
-  var geocoder = new google.maps.Geocoder();
-  var locationArray = getCoordinates();
-  // for (var i = 0; i < locationArray.length; i++) {
-  //   var lat = locationArray[i][0];
-  //   var lng = locationArray[i][1];
-  //   var latlng = new google.maps.LatLng(lat, lng);
-  //   geocoder.geocode({'latLng': latlng}, function(results, status) {
-  //     if (status == google.maps.GeocoderStatus.OK) {
-  //       if (results[1]) {
-
-  //       }
-  //     } else {
-  //       alert("Geocoder failed due to: " + status);
-  //     }
-  //   });
-  // };
+  var coordinates = getCoordinates();
+  // var locationArray = parseCoordinates(coordinates);
+  console.log(coordinates);
+  
 }
 
-codeLatLng();
+function revgeocode(locationArray){
+  var geocoder = new google.maps.Geocoder();
+  for (var i = 0; i < 5; i++) {
+    var lat = locationArray[i][0];
+    var lng = locationArray[i][1];
+    var latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        // if (results[1]) {
+          console.log(results[0]);
+          console.log(results[1]);
+          console.log(results[2]);
+        // }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
+}
+
+function parseCoordinates(coordinates){
+  
+  var latLong = [];
+  for(var i = 0; i < coordinates.length; i++){
+    latLong.push([coordinates[i].latitude, coordinates[i].longitude]);
+  }
+  console.log("parsing coordinates");
+  revgeocode(latLong);
+}
 
 function getCoordinates() {
+  
   $.ajax({
     url: "/rat_sightings.json",
     dataType: "JSON",
     success: function(response){
-      var latLong = [];
-      response.forEach(function(obj){
-        latLong.push([obj.latitude, obj.longitude])
-      });
-      console.log(latLong);
-    },
+      parseCoordinates(response);
+    }, 
     error: function(){alert("failure!");}
   });
 }
+
+$(document).ready( function() { codeLatLng();});
 
 // $.ajax({
 //   url: "/lists.json",
--- rattracker/app/models/rat_sighting.rb
+++ rattracker/app/models/rat_sighting.rb
@@ -1,9 +1,7 @@
 
 class RatSighting < ActiveRecord::Base
 
-<<<<<<< HEAD
 
-=======
   def self.zip_specific_sightings(params_zip)
     if params_zip == 0
       "That is not a valid zip code"
@@ -13,6 +11,6 @@ class RatSighting < ActiveRecord::Base
       end
     end
   end
->>>>>>> 129bf4d6587310959ee123e7aa4bd048566d3e61
+
 
 end
