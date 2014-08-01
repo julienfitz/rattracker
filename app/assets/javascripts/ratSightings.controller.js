3eec3dbc49be6efa886c611f5bca5712bca4e680
lizzerdrix <liz@lizbaillie.com>
2014-08-01 10:43:15 -0400

    deletes previous address listings on new address submit

--- app/assets/javascripts/ratSightings.controller.js
+++ app/assets/javascripts/ratSightings.controller.js
@@ -21,11 +21,12 @@ function codeAddress(address) {
 
 function geocodeToRails(results) {
   console.log(results);
+  var add_comp = results["address_components"];
   $.ajax({
     url: '/rat_sightings',
     type: 'POST',
     dataType: 'JSON',
-    data: {zipcode: results['address_components'][8]['long_name']},
+    data: {zipcode: add_comp[add_comp.length - 1]['long_name']},
     error: function(response){
       eval(response.responseText);
     }
